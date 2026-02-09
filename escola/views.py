1873a913139dd1adfaebb78b7668492a72bf7d42
gustavoHps126 <gustavo.pf.126@gmail.com>
2026-02-08 23:21:18 -0300

    Permicoes de usuarios

--- escola/views.py
+++ escola/views.py
@@ -3,10 +3,11 @@ from escola.serializers import EstudanteSerializer, CursoSerializar, MatriculaSe
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from escola.throttles import MatriculaAnonRateThrottle
 
 class EstudanteViewsSet(viewsets.ModelViewSet):
-    queryset = Estudante.objects.all()
+    queryset = Estudante.objects.all().order_by("id")
     serializer_class = EstudanteSerializer
 
 
@@ -15,20 +16,23 @@ class CursoViewsSet(viewsets.ModelViewSet):
     serializer_class = CursoSerializar
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
+    throttle_classes = [UserRateThrottle,MatriculaAnonRateThrottle]
+    http_method_names = ["get", "post"]
+
 
 
 class ListaMatriculaEstudante(generics.ListAPIView):
     def get_queryset(self):
-        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
         return queryset
 
     serializer_class = ListaMatriculasEstudantesSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
     def get_queryset(self):
-        queryset = Matricula.objects.filter(curos_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(curos_id=self.kwargs['pk']).order_by("id")
         return queryset
 
     serializer_class = ListaMatriculasCursoSerializer
